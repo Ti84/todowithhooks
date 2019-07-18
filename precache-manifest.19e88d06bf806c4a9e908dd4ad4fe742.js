205874b9e5222ef0a5150437ae45c4b802adf001
Shane4666 <sc897@nau.edu>
2019-07-18 11:25:17 -0700

    Updates

---
+++ precache-manifest.19e88d06bf806c4a9e908dd4ad4fe742.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e11b92cdc724a3b48af2792172e9080",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f18560317d3898c5e67b",
+    "url": "/static/css/main.24367497.chunk.css"
+  },
+  {
+    "revision": "f06fcb84e0c8c8d69295",
+    "url": "/static/js/2.36c0fa17.chunk.js"
+  },
+  {
+    "revision": "f18560317d3898c5e67b",
+    "url": "/static/js/main.71033acc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
